e056b3a205cae691164108c55711d2522ae7c917
sonoro1234 <sonoroster@gmail.com>
2025-01-08 13:02:50 +0100

    initial

---
+++ generator/cimplot3d_template.h
@@ -0,0 +1,18 @@
+#ifndef CIMPLOT3D_INCLUDED
+#define CIMPLOT3D_INCLUDED
+
+#include "cimgui.h"
+
+#ifdef CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include "imgui_structs.h"
+#else
+#endif // CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+
+#include "auto_funcs.h"
+
+
+#endif //CIMPLOT3D_INCLUDED
+
+
+
+
